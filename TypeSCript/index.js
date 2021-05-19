367d546dfb2f3dc27ef5d77bbfd5c344f2a5e864
shepijcanwu <shepijcanwu@tencent.com>
2021-05-19 10:11:23 +0800

    typescript

---
+++ TypeSCript/index.js
@@ -0,0 +1,13 @@
+var username = 'wczix';
+var age = 22;
+var nickname = '1';
+username = nickname;
+var person = {
+    username: 'wczix',
+    age: 1
+};
+var adult = {
+    username: 'wczix',
+    age: 1
+};
+var first = { first: 2 };
